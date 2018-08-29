721c7a38c6f8be4f0999fe4b774032945a46eded
Eldin Zenderink <s1077709@student.windesheim.nl>
2018-08-29 13:30:02 +0200

    Set nsis installer to single click.

--- Electron/buildwindows.bat
+++ Electron/buildwindows.bat
@@ -1,3 +1,3 @@
 call npm install
 call npm install electron
-call electron-builder build --config ./electron-builder.json --windows --x64
+call electron-builder build --config ./electron-builder.json --win --x64
